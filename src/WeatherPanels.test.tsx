7dc042ed5418cee0b698e832beb3da1e9e6fe5dd
TheGetitGuy <73693333+TheGetitGuy@users.noreply.github.com>
2022-08-31 16:07:37 -0400

    Changing app to weather panels

--- src/WeatherPanels.test.tsx
+++ src/WeatherPanels.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import WeatherPanels from './WeatherPanels';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<WeatherPanels />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/WeatherPanels.tsx
+++ src/WeatherPanels.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import getIcon from './getIcon'; 
-import './App.css';
+import './WeatherPanels.css';
 import WeatherCard from './WeatherCard';
 import {useEffect, useState} from "react";
 import { dayModel } from './AppInterfaces';
 
-function App():JSX.Element {
+function WeatherPanels():JSX.Element {
 
-  const [weather, setWeather] = useState<dayModel[]>()
- // const [weatherTemp, setWeatherTemp] = useState('70')
-  //const [date, setDate] = useState('')
-  //const [weatherIcon, setWeatherIcon] = useState(process.env.PUBLIC_URL+'/loading.svg')
- // const [weatherToQuery, setWeatherToQuery] = useState('dillonvale')
+  const [weather, setWeather] = useState<dayModel[]>() 
   const weatherToQuery = 'New York';
   useEffect(()=>{
     fetch(`https://server2-0.vercel.app/weather/request?queryWeather=${weatherToQuery}`)
@@ -21,12 +17,7 @@ function App():JSX.Element {
       })
       .then(response =>{
         const weatherArray: dayModel[] = response;
-        setWeather(weatherArray)
-       // const newDate = new Date(parseInt(response.currentConditions.datetimeEpoch)*1000)
-       // setWeatherDescription(response.description)
-       // setDate(newDate.toDateString())
-       // setWeatherTemp(response.currentConditions.temp)
-       // setWeatherIcon(getIcon(response.currentConditions.icon)) //grabs the string from the getIcon.ts file.
+        setWeather(weatherArray) 
       })
       .catch(err =>{
         console.log(err)
@@ -46,4 +37,4 @@ function App():JSX.Element {
   );
 }
 
-export default App;
+export default WeatherPanels;
