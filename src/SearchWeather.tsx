604768b4cdea94159caad95514e6f41f95af4658
TheGetitGuy <73693333+TheGetitGuy@users.noreply.github.com>
2022-08-31 16:07:21 -0400

    Changing App to WeatherPanels

---
+++ src/SearchWeather.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './WeatherPanels.css'; 
+import WeatherPanels from './WeatherPanels'
+
+function SearchWeather():JSX.Element{
+    return <WeatherPanels/>
+}
+
+export default SearchWeather
